70ca648679efc30315b649acca69ce3c1acc5833
Skybadger <mike@Skybadger.net>
2022-03-15 09:04:49 +0000

    updated to remove usernames and passwords before commit Added Progmem flags

--- SkybadgerStrings.h
+++ SkybadgerStrings.h
@@ -1,31 +1,41 @@
 #if !defined _SKYBADGER_STRINGS_H_
 #define _SKYBADGER_STRINGS_H_
+//Strings moved to PROGMEM to save on heap . Use care with accessing. 
+//ALPACA management interface Strings 
+const char* Manufacturer PROGMEM = "Skybadger Astronomy";
 
-//Strings
-const char* ssid1     =        "";
-const char* password1 =        "";
-
-const char* ssid2     =        "";
-const char* password2 =        "";
-
-const char* ssid3     =        "";
-const char* password3 =        "";
-
-const char* ssid4     =        "";
-const char* password4 =        "";
+//WiFi Strings
+const char* ssid1 		PROGMEM =        "";
+const char* password1 	PROGMEM =        "";
+const char* ssid2 		PROGMEM =        "";
+const char* password2 	PROGMEM =        "";
+const char* ssid3 		PROGMEM =        "";
+const char* password3 	PROGMEM =        "";
+const char* ssid4 		PROGMEM =        "";
+const char* password4 	PROGMEM =        "";
+const char* ssid5 		PROGMEM =        "";
+const char* password5 	PROGMEM =        "";
 
 //MQTT settings
-const char* pubsubUserID =    "";
-const char* pubsubUserPwd =   "";
-const char* mqtt_server =     "obbo.i-badger.co.uk";
-const char* outHealthTopic =  "/skybadger/devices/";
-const char* outSenseTopic =   "/skybadger/sensors/";
-const char* outFnTopic =      "/skybadger/functions/";
-const char* inTopic =         "/skybadger/devices/heartbeat";
+const char* pubsubUserID 	PROGMEM =   "";
+const char* pubsubUserPwd 	PROGMEM =   "";
+const char* mqtt_server 	PROGMEM =   "";
+const char* mqtt_server2 	PROGMEM =   "";
+
+//Use structure topic: /<MQTT host>/<feature>/<feature type>/<hostname>  payload:  json string of <values>
+const char* outHealthTopic 	PROGMEM =  	"/skybadger/devices/";
+const char* outSenseTopic 	PROGMEM =   "/skybadger/sensors/";
+const char* outFnTopic  	PROGMEM =   "/skybadger/functions/";
+const char* inTopic 		PROGMEM =   "/skybadger/devices/heartbeat";
 
 //Timeserver settings - UK 
+#if defined _USE_PROGMEM
+const char* timeServer1 PROGMEM = "0.uk.pool.ntp.org";
+const char* timeServer2 PROGMEM = "1.uk.pool.ntp.org";
+const char* timeServer3 PROGMEM = "ntp.exnet.com";
+#else
 const char* timeServer1 = "0.uk.pool.ntp.org";
 const char* timeServer2 = "1.uk.pool.ntp.org";
 const char* timeServer3 = "ntp.exnet.com";
-
+#endif
 #endif
